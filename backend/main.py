6341265bd08ded3397cbd65b635f1d2d2de8eb4b
hhoussaini <hoss@hope-advisory.com>
2025-02-23 18:03:20 +0000

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -26,12 +26,12 @@ class ScrapeResponse(BaseModel):
 # Function to set up Selenium WebDriver
 def get_driver():
     chrome_options = Options()
-    chrome_options.binary_location = "/usr/bin/google-chrome-stable"  # Explicit Chrome binary path
+    chrome_options.binary_location = CHROME_BINARY_PATH
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
 
-    service = Service("/usr/local/bin/chromedriver")  # Explicit ChromeDriver path
+    service = Service(CHROMEDRIVER_PATH)
     driver = webdriver.Chrome(service=service, options=chrome_options)
     return driver
 
