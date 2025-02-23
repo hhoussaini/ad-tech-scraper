6782318869a2a1c1df7da425679ca49218295b02
hhoussaini <hoss@hope-advisory.com>
2025-02-23 17:34:14 +0000

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -38,9 +38,11 @@ def get_driver():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
 
-    service = Service("/usr/local/bin/chromedriver")  # Explicitly set ChromeDriver path
+    # Explicitly define the path to ChromeDriver
+    service = Service("/usr/local/bin/chromedriver")  
     driver = webdriver.Chrome(service=service, options=chrome_options)
     return driver
+
 
 
 # Function to scrape technologies
